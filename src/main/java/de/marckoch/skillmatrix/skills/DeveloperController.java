a5bd58ff506c9fef04bcb9a2fa4a29b4c143e1e8
Marc Koch <mako.java@posteo.de>
2021-10-03 18:17:57 +0200

    move add skill to developer details view

--- src/main/java/de/marckoch/skillmatrix/skills/DeveloperController.java
+++ src/main/java/de/marckoch/skillmatrix/skills/DeveloperController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,12 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @AllArgsConstructor
 class DeveloperController {
 
 	private final DeveloperRepository developerRepository;
+
+	private final SkillRepository skillRepository;
 
 	@GetMapping("/developers")
 	public ModelAndView showAll() {
@@ -32,6 +37,13 @@ class DeveloperController {
 		ModelAndView mav = new ModelAndView("developers/developerDetails");
 		Developer dev = developerRepository.findById(developerId).get();
 		mav.addObject(dev);
+
+		Experience experience = new Experience();
+		experience.setDeveloper(dev);
+		mav.getModel().put("newExperience", experience);
+
+		mav.getModel().put("skillSelectItems", getFreeSkills(dev));
+
 		return mav;
 	}
 
@@ -69,5 +81,27 @@ class DeveloperController {
 			developerRepository.save(developer);
 			return "redirect:/developers/{developerId}";
 		}
+	}
+
+	public Map<Integer, String> getFreeSkills(Developer dev) {
+		var skillsOfDeveloper = dev.getExperiences()
+				.stream()
+				.map(exp -> exp.getSkill().getSkillId())
+				.toList();
+
+		return skillRepository.findAll().stream()
+				.filter(skill -> !skillsOfDeveloper.contains(skill.getSkillId()))
+				.collect(Collectors.toMap(
+						Skill::getSkillId,
+						skill -> skill.getName() + " " + skill.getVersion()
+				));
+	}
+
+	@ModelAttribute("ratings")
+	public Map<Integer, String> getRatings() {
+		return IntStream.range(1, 6).boxed().collect(Collectors.toMap(
+				Integer::valueOf,
+				String::valueOf
+		));
 	}
 }
--- src/main/java/de/marckoch/skillmatrix/skills/ExperienceController.java
+++ src/main/java/de/marckoch/skillmatrix/skills/ExperienceController.java
@@ -4,14 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 @AllArgsConstructor
@@ -21,44 +18,6 @@ class ExperienceController {
 
     private final DeveloperRepository developerRepository;
 
-    private final SkillRepository skillRepository;
-
-    public Map<Integer, String> getFreeSkills(Developer dev) {
-        var skillsOfDeveloper = dev.getExperiences()
-                .stream()
-                .map(exp -> exp.getSkill().getSkillId())
-                .toList();
-
-        return skillRepository.findAll().stream()
-                .filter(skill -> !skillsOfDeveloper.contains(skill.getSkillId()))
-                .collect(Collectors.toMap(
-                        Skill::getSkillId,
-                        skill -> skill.getName() + " " + skill.getVersion()
-                ));
-    }
-
-    @ModelAttribute("ratings")
-    public Map<Integer, String> getRatings() {
-        return IntStream.range(1, 6).boxed().collect(Collectors.toMap(
-                Integer::valueOf,
-                String::valueOf
-        ));
-    }
-
-    @GetMapping("/experience/{developerId}/new")
-    public String initCreationForm(Map<String, Object> model, @PathVariable("developerId") int developerId) {
-        Developer dev = developerRepository.findById(developerId).get();
-        model.put("developer", dev);
-
-        Experience experience = new Experience();
-        experience.setDeveloper(dev);
-        model.put("experience", experience);
-
-        model.put("skillSelectItems", getFreeSkills(dev));
-
-        return "/experience/createOrUpdateExperienceForm";
-    }
-
     @PostMapping("/experience/{developerId}/new")
     public String processCreationForm(Map<String, Object> model, @PathVariable("developerId") int developerId,
                                       @Valid Experience experience, BindingResult result) {
@@ -67,7 +26,7 @@ class ExperienceController {
         experience.setDeveloper(dev);
 
         if (result.hasErrors()) {
-            return "/experience/createOrUpdateExperienceForm";
+            return "redirect:/developers/" + dev.getDeveloperId();
         } else {
             experienceRepository.save(experience);
             return "redirect:/developers/" + dev.getDeveloperId();
