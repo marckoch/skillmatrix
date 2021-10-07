7ceec456a4a251d5a9f3aeeac68ba48ca0f3baa7
Marc Koch <mako.java@posteo.de>
2021-10-07 20:36:05 +0200

    make skill list sortable by name and add pagination

--- src/main/java/de/marckoch/skillmatrix/skills/SkillController.java
+++ src/main/java/de/marckoch/skillmatrix/skills/SkillController.java
@@ -1,16 +1,20 @@
 package de.marckoch.skillmatrix.skills;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.util.Collection;
 import java.util.Map;
 
 @Controller
@@ -20,11 +24,35 @@ class SkillController {
 	private final SkillRepository skillRepository;
 
 	@GetMapping("/skills")
-	public ModelAndView showAll() {
-		ModelAndView mav = new ModelAndView("skills/skillList");
-		Collection<Skill> skills = skillRepository.findAll();
-		mav.getModel().put("skills", skills);
-		return mav;
+	public String showAll() {
+		return "redirect:skills/page/0?sort-field=name&sort-dir=asc";
+	}
+
+	@GetMapping("/skills/page/{pagenumber}")
+	public String showAll(@PathVariable int pagenumber,
+						  @RequestParam(name = "sort-field") final String sortField,
+						  @RequestParam(name = "sort-dir") final String sortDir,
+						  Model model) {
+		Sort sort = ("desc".equalsIgnoreCase(sortDir)) ?
+				Sort.by(sortField).descending() :
+				Sort.by(sortField).ascending();
+
+		Pageable p = PageRequest.of(pagenumber, 5, sort);
+		Page<Skill> resultPage = skillRepository.findAll(p);
+
+		model.addAttribute("skills", resultPage);
+
+		// paging
+		model.addAttribute("currentPage", pagenumber);
+		model.addAttribute("totalPages", resultPage.getTotalPages());
+		model.addAttribute("totalItems", resultPage.getTotalElements());
+
+		// sorting
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", sortDir.equalsIgnoreCase("asc") ? "desc" : "asc");
+
+		return "skills/skillList";
 	}
 
 	@GetMapping("/skills/{skillId}")
