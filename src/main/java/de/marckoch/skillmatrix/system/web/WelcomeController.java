db656feac204c9232f783205799988f948451c5c
Marc Koch <mako.java@posteo.de>
2021-10-18 12:05:35 +0200

    add warning to welcome page when data is from H2

--- src/main/java/de/marckoch/skillmatrix/system/web/WelcomeController.java
+++ src/main/java/de/marckoch/skillmatrix/system/web/WelcomeController.java
@@ -1,13 +1,23 @@
 package de.marckoch.skillmatrix.system.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
+@AllArgsConstructor
 class WelcomeController {
 
+	private final Environment environment;
+
 	@GetMapping("/")
-	public String welcome() {
+	public String welcome(Model model) {
+		boolean noActiveProfile = Arrays.asList(environment.getActiveProfiles()).isEmpty();
+		model.addAttribute("showWarning", noActiveProfile);
 		return "welcome";
 	}
 
