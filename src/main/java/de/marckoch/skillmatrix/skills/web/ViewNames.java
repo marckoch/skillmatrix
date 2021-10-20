2aaf0f4d872db073ee8bfa371dbee626eb4bfb9d
Marc Koch <mako.java@posteo.de>
2021-10-20 16:04:55 +0200

    remove sonarcloud warning

--- src/main/java/de/marckoch/skillmatrix/skills/web/ViewNames.java
+++ src/main/java/de/marckoch/skillmatrix/skills/web/ViewNames.java
@@ -1,6 +1,10 @@
 package de.marckoch.skillmatrix.skills.web;
 
 public class ViewNames {
+    private ViewNames() {
+        // no instance
+    }
+
     public static final String DEVELOPER_DETAILS = "developers/developerDetails";
     public static final String DEVELOPER_LIST = "developers/developerList";
     public static final String CREATE_OR_UPDATE_DEVELOPER_VIEW = "developers/createOrUpdateDeveloperForm";
