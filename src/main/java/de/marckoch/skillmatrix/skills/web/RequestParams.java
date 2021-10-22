fc8a46aa5ea2cc640c8946b589cb24587ac9f8fd
Marc Koch <mako.java@posteo.de>
2021-10-22 07:33:55 +0200

    remove sonarcloud warnings

--- src/main/java/de/marckoch/skillmatrix/skills/web/RequestParams.java
+++ src/main/java/de/marckoch/skillmatrix/skills/web/RequestParams.java
@@ -1,6 +1,8 @@
 package de.marckoch.skillmatrix.skills.web;
 
 public class RequestParams {
+    private RequestParams() {}
+
     public static final String SORT_FIELD = "sort-field";
     public static final String SORT_DIR = "sort-dir";
 }
